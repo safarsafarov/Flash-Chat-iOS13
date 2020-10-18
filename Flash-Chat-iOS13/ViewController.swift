19af1f775c14a5e63082d7edd2c3c2a80832afee
safarsafarov <safarov.safar@hotmail.com>
2020-10-19 03:25:05 +0800

    new

---
+++ Flash-Chat-iOS13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash-Chat-iOS13
+//
+//  Created by Safar Safarov on 2020/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
